9a7a56884d19badfca84b06ba241ac6a83e77455
Mouba1 <167690279+Mouba1@users.noreply.github.com>
2026-06-06 23:16:41 -0400

    Added a new supplier

---
+++ 1_.sql
@@ -0,0 +1,4 @@
+use northwind;
+
+insert into suppliers (CompanyName, ContactName, ContactTitle, City, Country)
+values ('Mubarak Sandwich Hub', 'Mubarak Congacou', 'Owner', 'New York', 'United States');
